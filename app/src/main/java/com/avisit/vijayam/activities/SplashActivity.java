3809c906600e6333c00977abe4a6aedf961f5b0f
Siva Sankar Rajendran <siva.pcu@gmail.com>
2015-04-25 11:46:47 +0530

    For version 2

---
+++ app/src/main/java/com/avisit/vijayam/activities/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.avisit.vijayam.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+
+import com.avisit.vijayam.R;
+
+public class SplashActivity extends ActionBarActivity {
+    // Constant Value for Splash Screen time visibility
+    private static int SPLASH_SCREEN_TIME = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        // Create object of Handler class and call method postDelayed to make
+        // Splash Screen visible for SPLASH_SCREEN_TIME
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        }, SPLASH_SCREEN_TIME);
+    }
+}
